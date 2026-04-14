26293cc390d2a112b01e5d9295322411b4644220
Kishan Lokhil <lokhilkishan60@gmail.com>
2026-04-14 22:03:16 +0530

    Updated resume file name and Hero component link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,7 +27,7 @@ export default function Hero() {
 
           <div className="mt-8 flex justify-center md:justify-start animate-fadeIn animate-delay-400">
             <Link
-              href="/Kishan_lokhil.pdf"
+              href="/DevOps_Resume_Kishan_Lokhil.pdf"
               download
               className="flex items-center gap-2 px-6 py-3 border border-gray-300 dark:border-gray-700 rounded-lg transition bg-white dark:bg-black text-gray-900 dark:text-white hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black hover:scale-105"
             >
